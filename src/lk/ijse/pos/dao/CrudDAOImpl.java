6f0e00fda7334fd06f1401bc70301154b85d378d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2019-05-28 17:03:27 +0530

    finishes dao layer

--- src/lk/ijse/pos/dao/CrudDAOImpl.java
+++ src/lk/ijse/pos/dao/CrudDAOImpl.java
@@ -3,47 +3,47 @@ package lk.ijse.pos.dao;
 import lk.ijse.pos.entity.SuperEntity;
 import org.hibernate.Session;
 
+import javax.persistence.EntityManager;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 public abstract class CrudDAOImpl<T extends SuperEntity, ID extends Serializable> implements CrudDAO<T,ID>{
 
-    protected Session session;
+    protected EntityManager em;
     private Class<T> entity;
-
 
     public CrudDAOImpl(){
         entity = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 
     @Override
-    public void setSession(Session session) {
-        this.session = session;
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
     }
 
     @Override
     public void save(T entity) throws Exception {
-        session.save(entity);
+        em.persist(entity);
     }
 
     @Override
     public void update(T entity) throws Exception {
-        session.merge(entity);
+        em.merge(entity);
     }
 
     @Override
     public void delete(ID entityId) throws Exception {
-        session.delete(session.load(entity, entityId));
+        em.remove(em.getReference(entity, entityId));
     }
 
     @Override
     public List<T> findAll() throws Exception {
-        return session.createQuery("FROM " + entity.getName()).list();
+        return em.createQuery("SELECT alias FROM " + entity.getName() + " alias").getResultList();
     }
 
     @Override
     public T find(ID entityId) throws Exception {
-        return session.find(entity,entityId);
+        return em.find(entity,entityId);
     }
 }
